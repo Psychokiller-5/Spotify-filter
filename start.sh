6abf9ae4c2e3c894f05455690cce4ee05defeeb7
Psychokiller 5 <102171274+Psychokiller-5@users.noreply.github.com>
2022-08-12 21:16:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/Psychokiller-5/Spotify-filter.git /Spotify-filter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Spotify-filter
 fi
-cd /EvaMaria
+cd /Spotify-filter
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
