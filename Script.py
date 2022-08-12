dc5093b692026f30981efb37a8c84ee04a18ddb8
Psychokiller 5 <102171274+Psychokiller-5@users.noreply.github.com>
2022-08-12 21:32:20 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,13 +1,13 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>,\n🎶🎵 Welcome to Spotify Downloa 🎵🎶I can help you search 🔍, listen 🎧 and download 📱 songs easily using Spotify URLs and custom queries! You can directly send Spotify URLs of tracks, playlists and albums to download them.---\n\nUse Help to know about command usage..  😍"""
+    START_TXT = """Hello {},
+My name is <a href=https://t.me/{}>{}</a>,\n\n🎶🎵 Welcome to Spotify Downloa 🎵🎶I can help you search 🔍, listen 🎧 and download 📱 songs easily using Spotify URLs and custom queries! You can directly send Spotify URLs of tracks, playlists and albums to download them.---\n\nUse Help to know about command usage..  😍"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
 ✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: Psycho Killer
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
+✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: Programing
+✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: Python 3
+✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 🤖
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
